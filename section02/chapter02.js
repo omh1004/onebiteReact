f161c95e7885dc17a0f63e5bf46059425bb97aa2
omh1004 <ohchef2020@gmail.com>
2025-02-01 15:08:11 +0900

    truthyFalsy

---
+++ section02/chapter02.js
@@ -0,0 +1,31 @@
+// //단락평가 
+
+// function returnFalse(){
+    
+//     //falsy 값에도 적용 
+//     return false;
+// }
+
+// function returnTrue(){
+//     return true;
+// }
+
+// console.log(returnFlase() && returnTrue());
+// console.log(returnFlase() || returnTrue());
+
+
+//단락평가 활용사례
+// && 조건에서는 앞의 값이 trusy 이면 앞에값이 실행되고, 
+// 값으로 저장될때는 앞에 값이 저장되며, 
+// || 조건에서는 두값이 모두 트루이면 뒤에값 거짓이여도 뒤에값이 저장된다. ////////
+
+function printName(person){
+    
+    const name = person && person.name;
+
+    console.log(name || undefined);
+    
+}
+
+printName();
+printName({name : "오민현"});
